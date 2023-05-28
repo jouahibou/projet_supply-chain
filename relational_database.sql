7a5e0cb9b8bba598572e3164e5dbba2ea665282f
jouahibou <jouahibou@gmail.com>
2023-05-28 15:53:56 +0200

    formatage trustpilot_data

--- relational_database.sql
+++ relational_database.sql
@@ -1,16 +1,17 @@
 -- Active: 1684518215732@@127.0.0.1@3306@database
-CREATE TABLE Avis (
+CREATE TABLE Entreprises (
     Nom Varchar(255),
     Note DECIMAL(5,2),
     Nombre_avis INT,
-    Pourcentage_avis_excelents VARCHAR(255),
+    Avis_excelents DECIMAL(5,0),
     Domaine VARCHAR(255)
 );
 
 
-LOAD DATA LOCAL INFILE 'trustpilot_data.csv'
-INTO TABLE Avis
+LOAD DATA LOCAL INFILE 'trustpilot_data1.csv'
+INTO TABLE Entreprise
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
 
 SHOW VARIABLES LIKE 'secure_file_priv';
+select * from Entreprise ;
