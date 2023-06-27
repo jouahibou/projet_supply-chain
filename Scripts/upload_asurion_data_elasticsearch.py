53227edb04525b64da9a3e81beef54d98f3067f2
Kristof Szucs <a1040@MacBook-Pro-de-IT-6.local>
2023-06-27 13:14:30 +0200

    script to upload and query ES

--- Scripts/upload_asurion_data_elasticsearch.py
+++ Scripts/upload_asurion_data_elasticsearch.py
@@ -5,6 +5,6 @@ import csv
 es = Elasticsearch(hosts = "http://@localhost:9200")
 
 
-with open('/home/ubuntu/projet_supply-chain/Data/asurion_preprocess.csv', encoding='utf-8') as f:
+with open('/home/ubuntu/projet_supply-chain/Data/asurion_complete.csv', encoding='utf-8') as f:
     reader = csv.DictReader(f)
-    helpers.bulk(es, reader, index='asurion_preprocessed')
+    helpers.bulk(es, reader, index='asurion')
